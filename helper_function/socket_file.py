4333ff07cca381ec93a5a3fc8f2daf9ed5c6d3bd
dairoemma <2632814@dundee.ac.uk>
2025-03-28 20:51:09 +0000

    almost done

--- helper_function/socket_file.py
+++ helper_function/socket_file.py
@@ -1,50 +1,50 @@
-from flask_socketio import SocketIO, emit, join_room, leave_room
-from flask import request as flask_request
+# from flask_socketio import SocketIO, emit, join_room, leave_room
+# from flask import request as flask_request
 
-socket = None
-active_users = {}
+# socket = None
+# active_users = {}
 
-def initialize_socket(app):
-    global socket
-    socket = SocketIO(app, cors_allowed_origins="*")
-    register_socket_events()
-    return socket
+# def initialize_socket(app):
+#     global socket
+#     socket = SocketIO(app, cors_allowed_origins="*")
+#     register_socket_events()
+#     return socket
 
-def register_socket_events():
-    @socket.on('connect')
-    def handle_connect():
-        print(f"Client connected: {flask_request.sid}")
-        emit('connection_success', {'message': 'Connected to WebSocket'})
+# def register_socket_events():
+#     @socket.on('connect')
+#     def handle_connect():
+#         print(f"Client connected: {flask_request.sid}")
+#         emit('connection_success', {'message': 'Connected to WebSocket'})
 
-    @socket.on('join_room')
-    def handle_join(data):
-        username = data.get("username")
-        if not username:
-            emit('error', {'message': 'Missing username'})
-            return
-        join_room(username)
-        active_users[flask_request.sid] = username
-        print(f"{username} joined room")
-        emit('joined_room', {'message': f"{username} joined"}, room=username)
+#     @socket.on('join_room')
+#     def handle_join(data):
+#         username = data.get("username")
+#         if not username:
+#             emit('error', {'message': 'Missing username'})
+#             return
+#         join_room(username)
+#         active_users[flask_request.sid] = username
+#         print(f"{username} joined room")
+#         emit('joined_room', {'message': f"{username} joined"}, room=username)
 
-    @socket.on('send_message')
-    def handle_send_message(data):
-        msg = data.get("message")
-        room = data.get("room")
-        if not msg or not room:
-            emit('error', {'message': 'Missing message or room'})
-            return
-        emit('receive_message', msg, room=room)
+#     @socket.on('send_message')
+#     def handle_send_message(data):
+#         msg = data.get("message")
+#         room = data.get("room")
+#         if not msg or not room:
+#             emit('error', {'message': 'Missing message or room'})
+#             return
+#         emit('receive_message', msg, room=room)
 
-    @socket.on('disconnect')
-    def handle_disconnect():
-        sid = flask_request.sid
-        username = active_users.pop(sid, None)
-        print(f"Client disconnected: {sid} ({username})")
+#     @socket.on('disconnect')
+#     def handle_disconnect():
+#         sid = flask_request.sid
+#         username = active_users.pop(sid, None)
+#         print(f"Client disconnected: {sid} ({username})")
 
-    @socket.on('leave_room')
-    def handle_leave(data):
-        room = data.get("room")
-        if room:
-            leave_room(room)
-            emit('left_room', {'message': f"Left room {room}"})
+#     @socket.on('leave_room')
+#     def handle_leave(data):
+#         room = data.get("room")
+#         if room:
+#             leave_room(room)
+#             emit('left_room', {'message': f"Left room {room}"})
